96e968d0448df655ff9d69dd72ad91eca9ffa35e
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-04-22 19:13:21 +0300

    Header setcion added

---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Алексей on 19.04.2017.
+ */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import "../assets/styles/components/Header.css";
+
+class Header extends Component {
+
+    // constructor(props){
+    //     super(props);
+    // };
+
+
+    render(){
+        return(
+            <header className="main">
+                <section className="progress__wrap">
+                    <div className="progress">
+                        <div className="tariff">{this.props.tariff}</div>
+                        <div className="in_days"> <b>{this.props.days.used}</b> из <b>{this.props.days.common}</b> { this.props.days.common%10 === 1 ? "дня" : "дней" }</div>
+                    </div>
+
+                    <div className="progress_bar">
+                        <div className="fill" style={{width: this.props.days.used/this.props.days.common * 100+"%" }}> </div>
+                    </div>
+
+                </section>
+                <button id="exit">e</button>
+            </header>
+        )
+    }
+}
+
+Header.defaultProps = {
+    tariff: "бесплатный",
+    days: {used: 10, common: 30}
+}
+Header.propTypes = {
+    days: PropTypes.shape({
+        used: PropTypes.number,
+        common: PropTypes.number
+    })
+}
+
+export default Header;
