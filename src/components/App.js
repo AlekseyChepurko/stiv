7b55e0a92b8b348c731185a8563539ac2a3d1b7c
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-04-24 13:19:30 +0300

    children props added

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import SideMenu from './SideMenu';
-import Header from './Header'
-import '../assets/styles/components/defaults.css';
+import SideMenu from './common/SideMenu';
+import Header from './common/Header'
+import '../assets/styles/components/common/defaults.css';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
           <SideMenu />
+
           <section className="content">
-          <Header />
+            <Header />
+              <main>
+                  {this.props.children}
+              </main>
           </section>
       </div>
     );
