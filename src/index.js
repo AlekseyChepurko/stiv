a32ff2b46e1060596e131cb0c81667cd5f0f9847
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-04-24 13:28:31 +0300

    index page promo and forum sections added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import promoLogoImg from '../public/assets/img/promo_logo.png';
 import "./assets/styles/normolize.css"
-import App from './components/App';
 import './assets/styles/index.css';
+import App from './components/App';
 
 ReactDOM.render(
-  <App />,
+    <App>
+            <section className="left_wrapper">
+                <section className="promo">
+                    <img src={promoLogoImg} className="promo_img" alt="promo_logo"></img>
+                    <section className="promo_content">
+                        <form action="#" className="promo_form">
+                            <input type="text" id="promo_number" name="promo_number" placeholder="Введите промокод"/>
+                            <button type="submit">активировать</button>
+                        </form>
+                        <div className="promo_notice">Если у вас есть промо код, введите его в форму ниже. Активация промо кода возможна только если у вас тариф  "Бесплатный".</div>
+                    </section>
+                </section>
+
+                <section className="forum">
+                    <div className="content">
+                        <header>форум</header>
+                        <div className="text">Если у вас есть промо код, введите его в форму ниже. Возможна только если у вас тариф  "Бесплатный".</div>
+                    </div>
+                    <button>
+                        <a href="#">
+                            присоединиться
+                        </a>
+                    </button>
+                </section>
+            </section>
+
+            <section className="right_wrapper">right</section>
+    </App>,
   document.getElementById('root')
 );
