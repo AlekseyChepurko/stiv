b9842dfd8579e96fdc85483da0ef26ca4e1f97f9
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-04-26 11:38:25 +0300

    newitem addition as object with custom props

--- src/components/NewsComponent.js
+++ src/components/NewsComponent.js
@@ -2,14 +2,14 @@
  * Created by chep on 24.04.17.
  */
 import React, { Component } from 'react';
+import NewsItem from './NewsItem';
 import "../assets/styles/normolize.css"
 
 class NewsComponent extends Component{
     constructor(props){
         super(props);
-
         this.state = {
-            dropdownState: false,
+            currentPage: this.props.currentPage,
         };
 
         this.updateDropdownState = this.updateDropdownState.bind(this);
@@ -19,13 +19,35 @@ class NewsComponent extends Component{
         this.setState({dropdownState: !this.state.dropdownState});
     }
 
+
     render(){
+
+        let items = [];
+
+        this.props.news.forEach((newsItem,iterator) => {
+
+            let toRender = React.createElement(NewsItem, {key: iterator});
+            const props = {};
+            for (let i in newsItem){
+                Object.defineProperty(props, i, {value: newsItem[i], enumerable: true});
+            }
+             toRender = React.cloneElement(toRender, props);
+            items.push(toRender);
+        })
+
         return(
-            <li className="">
-                bla bla
-            </li>
+            <ul className="news_wrapper">
+                { items }
+            </ul>
         );
     };
+}
+
+NewsComponent.defaultProps = {
+    newsPerPage: 3,
+    currentPage: 1,
+    // props for news item: title imgDir imgName creationDate link text
+    news: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
 }
 
 export default NewsComponent;
