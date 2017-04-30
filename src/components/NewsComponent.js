6b6f58870f2fabf4ddb5cca955115a5114b06ebd
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-04-30 18:22:25 +0300

    tab switch added

--- src/components/NewsComponent.js
+++ src/components/NewsComponent.js
@@ -6,13 +6,6 @@ import NewsItem from './NewsItem';
 import "../assets/styles/normolize.css"
 
 class NewsTab extends Component{
-    constructor(props){
-        super(props);
-
-        this.state = {
-            active: this.props.active ? "active": " ",
-        }
-    }
     render(){
         const items = [];
         this.props.items.forEach((item, iter) => {
@@ -22,7 +15,7 @@ class NewsTab extends Component{
         });
 
         return(
-            <ul className={`news_tab tab_num_ ${this.props.num} ${this.state.active }` } >
+            <ul className={`news_tab tab_num_ ${this.props.num} ${this.props.active ? "active" : "" }` } >
                 { items }
             </ul>
         );
@@ -52,7 +45,7 @@ class NewsTabs extends Component{
         tabs.forEach( (item, iter) => {
                 tabsRender.push(
                     React.createElement(NewsTab, {items: item, key:iter, num: iter+1, active: (iter+1) === this.props.currentPage ? true : false })
-                )
+    )
             }
         );
 
@@ -61,6 +54,48 @@ class NewsTabs extends Component{
                 { tabsRender }
             </ul>
         );
+    }
+}
+
+class TabControl extends Component {
+    constructor(props){
+        super(props);
+
+        this.setCurrentPage = this.setCurrentPage.bind(this.props.that);
+    }
+
+    setCurrentPage(event){
+        this.setState({currentPage: Number(event.target.value)});
+    };
+    render(){
+        const inputs = [];
+        if(this.props.currentPage > 1)
+            inputs.push(
+                <label htmlFor="prev_tab" key="prev_tab" >
+                    {this.props.currentPage - 1}
+                    <input type="radio" className="tab_control" id="prev_tab" name="tab_control_item" value={this.props.currentPage - 1} onClick={this.setCurrentPage}/>
+                </label>
+            );
+        inputs.push(
+            <label htmlFor="current_tab" key="current_tab">
+                {this.props.currentPage}
+                <input type="radio" className="tab_control" id="current_tab" name="tab_control_item" value={this.props.currentPage} checked={true} onClick={this.setCurrentPage}/>
+            </label>
+        );
+        if (this.props.currentPage < this.props.pageCount)
+            inputs.push(
+                <label htmlFor="next_tab" key="next_tab">
+                    {this.props.currentPage + 1}
+                    <input type="radio" className="tab_control" id="next_tab" name="tab_control_item" value={this.props.currentPage + 1} onClick={this.setCurrentPage}/>
+                </label>
+            );
+
+
+        return(
+            <div className="tab_nuber_selector">
+                { inputs }
+            </div>
+        )
     }
 }
 
@@ -73,17 +108,11 @@ class NewsComponent extends Component{
 
     }
 
-    updateCurentPage(num) {
-        this.setState({currentPage: num})
-    }
-
     render(){
         return(
             <section className="news">
-                <NewsTabs news={this.props.news} newsPerPage={this.props.newsPerPage} currentPage={this.props.currentPage}/>
-                <div className="tab_nuber_selector">
-                    {/*{ inputs }*/}
-                </div>
+                <NewsTabs news={this.props.news} newsPerPage={this.props.newsPerPage} currentPage={this.state.currentPage}/>
+                <TabControl currentPage={this.state.currentPage} that={this} pageCount={ Math.ceil(this.props.news.length/this.props.newsPerPage) } />
             </section>
 
         );
@@ -92,7 +121,7 @@ class NewsComponent extends Component{
 
 NewsComponent.defaultProps = {
                             newsPerPage: 3,
-                            currentPage: 2,
+                            currentPage: 3,
                             paeToShow: 3,
                             // props for news item: title, imgDir, imgName, creationDate, link, text
                             news: [{first: "first"}, {}, {}, {}, {}, {}, {}, {}, {}, {last:"last"}]
