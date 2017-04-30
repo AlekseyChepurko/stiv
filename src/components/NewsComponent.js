84a343d1c27cb20323f7f92633a860128be88170
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-04-30 03:01:12 +0300

    tabs structure added

--- src/components/NewsComponent.js
+++ src/components/NewsComponent.js
@@ -5,6 +5,65 @@ import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 import "../assets/styles/normolize.css"
 
+class NewsTab extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            active: this.props.active ? "active": " ",
+        }
+    }
+    render(){
+        const items = [];
+        this.props.items.forEach((item, iter) => {
+            items.push(
+                React.createElement(NewsItem, {item, key: iter})
+            );
+        });
+
+        return(
+            <ul className={`news_tab tab_num_ ${this.props.num} ${this.state.active }` } >
+                { items }
+            </ul>
+        );
+    }
+}
+
+class NewsTabs extends Component{
+    constructor(props){
+            super(props);
+
+            this.state = {
+                active: this.props.active ? "active": " ",
+            }
+        }
+
+    render(){
+        const tabs = [],
+              tabsRender = [];
+
+        this.props.news.forEach( (item, iter) => {
+            const index = Math.floor(iter/this.props.newsPerPage);
+            if(tabs[index] === undefined)
+                tabs[index] = [];
+            tabs[index].push(item);
+        })
+
+        tabs.forEach( (item, iter) => {
+                tabsRender.push(
+                    React.createElement(NewsTab, {items: item, key:iter, num: iter+1, active: (iter+1) === this.props.currentPage ? true : false })
+                )
+            }
+        );
+
+        return(
+            <ul className="news_wrapper">
+                { tabsRender }
+            </ul>
+        );
+    }
+}
+
 class NewsComponent extends Component{
     constructor(props){
         super(props);
@@ -12,42 +71,38 @@ class NewsComponent extends Component{
             currentPage: this.props.currentPage,
         };
 
-        this.updateDropdownState = this.updateDropdownState.bind(this);
     }
 
-    updateDropdownState(){
-        this.setState({dropdownState: !this.state.dropdownState});
+    updateCurentPage(num) {
+        this.setState({currentPage: num})
     }
-
 
     render(){
-
-        let items = [];
-
-        this.props.news.forEach((newsItem,iterator) => {
-
-            let toRender = React.createElement(NewsItem, {key: iterator});
-            const props = {};
-            for (let i in newsItem){
-                Object.defineProperty(props, i, {value: newsItem[i], enumerable: true});
-            }
-             toRender = React.cloneElement(toRender, props);
-            items.push(toRender);
-        })
-
         return(
-            <ul className="news_wrapper">
-                { items }
-            </ul>
+            <section className="news">
+                <NewsTabs news={this.props.news} newsPerPage={this.props.newsPerPage} currentPage={this.props.currentPage}/>
+                <div className="tab_nuber_selector">
+                    {/*{ inputs }*/}
+                </div>
+            </section>
+
         );
     };
 }
 
+NewsTabs.defaultProps = {
+    num: 0,
+    active: false,
+}
+
+
 NewsComponent.defaultProps = {
-    newsPerPage: 3,
-    currentPage: 1,
-    // props for news item: title imgDir imgName creationDate link text
-    news: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+                            newsPerPage: 3,
+                            currentPage: 2,
+                            paeToShow: 3,
+
+                            // props for news item: title, imgDir, imgName, creationDate, link, text
+                            news: [{first: "first"}, {}, {}, {}, {}, {}, {}, {}, {}, {last:"last"}]
 }
 
 export default NewsComponent;
