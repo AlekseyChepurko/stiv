a1e5a98c1e90d88ec67133d5748a3f46d1822795
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-05-05 02:05:42 +0300

    moved to routes

--- src/index.js
+++ src/index.js
@@ -1,51 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import promoLogoImg from '../public/assets/img/promo_logo.png';
 import "./assets/styles/normolize.css"
 import './assets/styles/index.css';
-import App from './components/App';
-import NewsComponent from "./components/NewsComponent";
-import Statistics from "./components/Statistics";
+import {
+    BrowserRouter as Router,
+    Route,
+    // TODO redirect settings
+    Redirect
+} from 'react-router-dom';
+
+
+import Profile from "./components/LoggedIn/Profile";
+
+import Registration from "./components/LoggedOut/Registration";
+import ForgotPass from "./components/LoggedOut/ForgotPass";
+import Login from "./components/LoggedOut/Login";
+
+const user = {
+    loggedIn: false
+};
 ReactDOM.render(
-    <App>
-            <section className="left_wrapper">
-                <section className="promo">
-                    <img src={promoLogoImg} className="promo_img" alt="promo_logo"></img>
-                    <section className="promo_content">
-                        <form action="#" className="promo_form">
-                            <input type="text" id="promo_number" name="promo_number" placeholder="Введите промокод"/>
-                            <button type="submit">активировать</button>
-                        </form>
-                        <div className="promo_notice">Если у вас есть промо код, введите его в форму ниже. Активация промо кода возможна только если у вас тариф  "Бесплатный".</div>
-                    </section>
-                </section>
+    <Router >
+        <div className="router">
+            <Route exact path="/" component={Profile} />
 
-                <section className="forum">
-                    <div className="content">
-                        <header>форум</header>
-                        <div className="text">Если у вас есть промо код, введите его в форму ниже. Возможна только если у вас тариф  "Бесплатный".</div>
-                    </div>
-                    <button>
-                        <a href="#">
-                            присоединиться
-                        </a>
-                    </button>
-                </section>
+            <Route exact path="/login" component={Login} />
 
-                <Statistics/>
-            </section>
+            <Route exact path="/registration" component={Registration} />
 
-            <section className="right_wrapper">
-                <section className="video">
-                    {/* some content such video should be here */}
-                    <button className="play"></button>
-                </section>
-
-                        <NewsComponent />
-
-            </section>
-    </App>,
+            <Route exact path="/forgot_pass" component={ForgotPass} />
+        </div>
+    </Router>,
   document.getElementById('root')
 )
-
-
