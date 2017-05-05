3adbad30c4ab7474804a66eb7c91aa3d6271ef97
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-05-05 13:55:36 +0300

    styles and pageName props added

--- src/components/common/Common.js
+++ src/components/common/Common.js
@@ -12,13 +12,13 @@ import '../../assets/styles/components/common/defaults.css'
 export default class Common extends Component {
     render() {
         return(
-            <section className="logged_in_info">
+            <section className={ `logged_in ${this.props.pageName ? this.props.pageName : ''}` } >
                 <SideMenu />
 
                 <section className="content">
                     <Header/>
 
-                    <main >
+                    <main style={ this.props.styles ? this.props.styles : {}}>
                         { this.props.children }
                     </main>
                 </section>
