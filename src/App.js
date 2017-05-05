5b0a54d6b4b79e40ace398d8f6f83e0efd50cfba
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-05-05 09:21:20 +0300

    file structure refator

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './assets/styles/components/common/defaults.css';
+
+
+class App extends Component {
+  render() {
+    return (
+        <div className="asd"></div>
+    );
+  }
+}
+
+export default App;
