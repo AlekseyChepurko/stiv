1c1537ea70c8bed018efc9d7e689f66d6e9bd11e
chep <aleksey.chepurko@gmail.com>
2017-05-11 02:57:56 +0300

    input form init state

---
+++ src/components/LoggedIn/AddAccount/SingleAccountAddition/InputForm.js
@@ -0,0 +1,15 @@
+/**
+ * Created by chep on 10.05.17.
+ */
+
+import React, {Component} from 'react'
+
+export default class InputForm extends Component {
+    render(){
+        return(
+            <form action="">
+
+            </form>
+        )
+    }
+}
