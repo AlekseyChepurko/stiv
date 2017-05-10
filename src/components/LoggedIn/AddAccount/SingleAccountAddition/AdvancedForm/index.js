2dc76b853ed78e674ef9019d198b77f3d4a5fe88
chep <aleksey.chepurko@gmail.com>
2017-05-11 02:56:01 +0300

    advanced form component init

---
+++ src/components/LoggedIn/AddAccount/SingleAccountAddition/AdvancedForm/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by chep on 10.05.17.
+ */
+import React, {Component} from 'react'
+
+export default class AdvancedForm extends Component {
+    render(){
+        return(
+            <form action=""></form>
+        )
+    }
+}
