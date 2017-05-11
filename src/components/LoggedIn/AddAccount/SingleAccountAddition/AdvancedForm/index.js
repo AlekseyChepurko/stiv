286d58a2f529891e743ee44c782e271c5c109a95
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-05-11 19:44:30 +0300

    default fields and themes added

--- src/components/LoggedIn/AddAccount/SingleAccountAddition/AdvancedForm/index.js
+++ src/components/LoggedIn/AddAccount/SingleAccountAddition/AdvancedForm/index.js
@@ -2,11 +2,66 @@
  * Created by chep on 10.05.17.
  */
 import React, {Component} from 'react'
+import InputForm from '../InputForm'
+
+const fields = [
+    {
+        type: "text",
+        placeholder: "Введите логин",
+        name: "login"
+    },
+    {
+        type: "password",
+        placeholder: "Введите пароль",
+        name: "password"
+    },
+    {
+        type: "text",
+        placeholder: "Прокси",
+        name: "proxy"
+    },
+    {
+        type: "phone",
+        placeholder: "Номер телефона",
+        name: "phone"
+    },
+    {
+        type: "text",
+        placeholder: "SmsActicate",
+        name: "SmsActicate"
+    },
+    {
+        type: "text",
+        placeholder: "UserID",
+        name: "UserID"
+    }
+];
+
+const themes = [
+    {
+        value: "Тематика1",
+        label:"Тематика аккаунта"},
+    {
+        value: "Тематика2",
+        label:"Тематика аккаунта"},
+    {
+        value: "Тематика3",
+        label:"Тематика аккаунта"},
+    {
+        value: "Тематика4",
+        label:"Тематика аккаунта"},
+    {
+        value: "Тематика5",
+        label:"Тематика аккаунта"},
+    {
+        value: "Тематика6",
+        label:"Тематика аккаунта"},
+];
 
 export default class AdvancedForm extends Component {
     render(){
         return(
-            <form action=""></form>
+            <InputForm fields={fields} themes={themes}/>
         )
     }
 }
