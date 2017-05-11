685b9e7d941cd7f734ef265ba827f0415e46e2b7
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-05-11 19:44:04 +0300

    deafult fields and themes added

--- src/components/LoggedIn/AddAccount/SingleAccountAddition/SimpleFrom/index.js
+++ src/components/LoggedIn/AddAccount/SingleAccountAddition/SimpleFrom/index.js
@@ -5,14 +5,44 @@
 import React, {Component} from 'react'
 import InputForm from '../InputForm'
 
+const fields = [
+    {
+        type: "text",
+        placeholder: "Введите логин",
+        name: "login"
+    },
+    {
+        type: "password",
+        placeholder: "Введите пароль",
+        name: "password"
+    }
+];
+
+const themes = [
+    {
+        value: "Тематика1",
+        label:"Тематика аккаунта"},
+    {
+        value: "Тематика2",
+        label:"Тематика аккаунта"},
+    {
+        value: "Тематика3",
+        label:"Тематика аккаунта"},
+    {
+        value: "Тематика4",
+        label:"Тематика аккаунта"},
+    {
+        value: "Тематика5",
+        label:"Тематика аккаунта"},
+    {
+        value: "Тематика6",
+        label:"Тематика аккаунта"},
+];
+
 export default class SimpleForm extends Component {
     render(){
         return(
-            <form action="#">
-                <input type="text" name="name"/>
-                <input type="password" id="password" />
-
-            </form>
+            <InputForm fields={fields} themes={themes}/>
         )
     }
 }
