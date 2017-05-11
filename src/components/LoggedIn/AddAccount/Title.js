5dc53ba666289778541a880c03db23d2071c7e94
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-05-11 19:41:00 +0300

    component added

--- src/components/LoggedIn/AddAccount/Title.js
+++ src/components/LoggedIn/AddAccount/Title.js
@@ -1,3 +1,28 @@
 /**
  * Created by chep on 10.05.17.
  */
+
+import React, {Component} from 'react'
+
+const titleText = "Добавьте <b>Instagram</b> аккаунт, чтобы начать его наполнение и расскрутку";
+const styles = {
+    color: "white",
+    fontSize: 21,
+    maxWidth: 395,
+    textAlign: "center",
+    marginBottom: 30,
+    marginTop: 30
+}
+
+export default class Title extends Component {
+    render(){
+        return(
+            <section
+                className="addition_title"
+                style={styles}
+                dangerouslySetInnerHTML={{__html: titleText}}
+            >
+            </section>
+        )
+    }
+}
