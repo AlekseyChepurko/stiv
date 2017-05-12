60d416c4e4cffb2a3f1dcfd7a4871dc373d7a6ff
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-05-13 02:42:14 +0300

    initial state of component

---
+++ src/components/LoggedIn/AccountFill/Left/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by chep on 12.05.17.
+ */
+
+import React, {Component} from 'react'
+
+export default class Left extends Component {
+    render(){
+        return(
+            <section></section>
+        )
+    }
+}
