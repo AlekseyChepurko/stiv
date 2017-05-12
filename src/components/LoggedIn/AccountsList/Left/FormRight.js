1ccb7ff393b04a9e9008e06888099718ae72b60d
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-05-12 04:05:21 +0300

    unsubscribed and info components imported

--- src/components/LoggedIn/AccountsList/Left/FormRight.js
+++ src/components/LoggedIn/AccountsList/Left/FormRight.js
@@ -2,12 +2,19 @@
  * Created by Алексей on 11.05.2017.
  */
 import React, {Component} from 'react'
+import FormInfo from './FormInfo'
+import FormUnsubscribe from './FormUnsubscribe'
+
+const style = {
+    minWidth: 403
+};
 
 export default class FormRight extends Component {
     render(){
         return(
-            <section>
-
+            <section style={style}>
+                <FormInfo/>
+                <FormUnsubscribe/>
             </section>
         )
     }
