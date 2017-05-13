059985fc990053d9f4c0747e5f9f366e8e9de8b7
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-05-13 21:47:15 +0300

    fields added

--- src/components/LoggedIn/AccountFill/Left/FormAdvanced/index.js
+++ src/components/LoggedIn/AccountFill/Left/FormAdvanced/index.js
@@ -1,12 +1,104 @@
-/**
- * Created by Алексей on 13.05.2017.
- */
 import React, {Component} from 'react'
+import Form from '../Form'
+
+const leftFields = [
+    {
+        type: "text",
+        placeholder: "Логин",
+        label: "Логин:",
+        name: "login"
+    },
+    {
+        type: "password",
+        placeholder: "Пароль",
+        label: "Пароль",
+        name: "password"
+    },
+    {
+        type: "text",
+        placeholder: "Введите прокси",
+        label: "Прокси:",
+        name: "proxy",
+    },
+    {
+        type: "phone",
+        placeholder: "",
+        label: "Номер телефона:",
+        name: "phone"
+    },
+    {
+        type: "text",
+        placeholder: "Введите активацию",
+        label: "SmsActivate",
+        name: "smsActivate",
+    },
+    {
+        type: "number",
+        placeholder: "",
+        label: "UserID",
+        name: "userID",
+    },
+    {
+        type: "textarea",
+        placeholder: "Введите конурентов",
+        label: "Списко конкурентов:",
+        name: "rivalList",
+        minHeight: 130
+    },
+    {
+        type: "number",
+        placeholder: "Введите число",
+        label: "Кол-во постов:",
+        name: "postNumber",
+    },
+    {
+        type: "number",
+        placeholder: "Введите число",
+        label: "Коэфф. in-езности:",
+        name: "INCoefficient",
+    },
+
+];
+
+const rightFields = [
+    {
+        type: "textarea",
+        placeholder: "Введите стоп-слова",
+        label: "Стоп-слова",
+        labelWidth: 115,
+        minHeight: 130,
+        name: "stopWords"
+    },
+    {
+        type: "number",
+        placeholder: "Введите число",
+        label: "Максимум тегов:",
+        labelWidth: 115,
+        name: "tagNumber",
+    },
+    {
+        type: "textarea",
+        placeholder: "Введите теги",
+        label: "Список тегов:",
+        labelWidth: 115,
+        name: "tagList",
+        minHeight: 250
+    },
+    {
+        type: "time",
+        placeholder: "",
+        label: "Время постинга:",
+        labelWidth: 115,
+        name: "tagMaximum"
+    },
+];
 
 export default class FromAdvanced extends Component {
     render(){
         return(
-            <section>advanced</section>
+            <Form
+                leftFields={leftFields}
+                rightFields={rightFields} />
         )
     }
 }
