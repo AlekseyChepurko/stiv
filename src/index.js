cac17a82b8a72b547916947beb38811f8c6c56ca
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-05-14 02:21:54 +0300

    /rival_subscribe route added

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ import AccountSettings from "./components/LoggedIn/AccountSettings";
 import StatisticsContent from "./components/LoggedIn/StatisticsContent";
 import AccountsList from "./components/LoggedIn/AccountsList";
 import AccountFill from "./components/LoggedIn/AccountFill";
+import RivalSubcribe from "./components/LoggedIn/RivalSubscribe"
 
 import Registration from "./components/LoggedOut/Registration";
 import ForgotPass from "./components/LoggedOut/ForgotPass";
@@ -34,6 +35,7 @@ ReactDOM.render(
             <Route exact path="/add_account" component={AddAccount} />
             <Route exact path="/accounts_list" component={AccountsList} />
             <Route exact path="/account_fill" component={AccountFill} />
+            <Route exact path="/rival_subscribe" component={RivalSubscribe} />
 
 
             <Route exact path="/login" component={Login} />
