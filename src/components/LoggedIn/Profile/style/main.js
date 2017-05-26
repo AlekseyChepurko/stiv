b9680138ac6783c2976b01b31132f2e3194c5a69
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-05-26 22:02:56 +0300

    styles fized

---
+++ src/components/LoggedIn/Profile/style/main.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Алексей on 26.05.2017.
+ */
+
+import {mainColors} from 'Common/style/variables'
+export const profileStyle = {
+    main: {
+        display: "flex",
+        flexDirection: "row"
+    },
+    left: {
+        maxWidth: 780,
+        margin: 0,
+        marginRight: 15,
+
+    },
+    right: {
+
+    }
+};
