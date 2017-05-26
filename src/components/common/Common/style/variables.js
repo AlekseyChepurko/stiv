cbf649e85f9e1374a8e9921e047c8076a8b189ca
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-05-26 21:59:56 +0300

    common variables added

---
+++ src/components/common/Common/style/variables.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Алексей on 26.05.2017.
+ */
+
+export const sideMenuColors ={
+    background: "#172437",
+    textDefault: "#c0c3c9",
+    textUsernmae: "#0680c6",
+    linkActive: "#ffffff",
+    linkNotActive: "#83868c"
+};
+
+export const transitionTime = 0.3;
+
+export const headerHeight = 75;
+
+// $imagePath: "../../../public/assets/img/"
+
+export const mainColors = {
+    background: '#ecf1f4',
+    blue: '#00a2fb',
+    sectionBorder: '#dce2e5 '
+};
+
+export const sideMenuWidth = 300;
