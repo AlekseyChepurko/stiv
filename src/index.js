d714315496b214dd25db4183c9e6e2424d41f89b
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-06-02 03:00:25 +0300

    404 element added

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import './assets/styles/index.css';
 import {
     BrowserRouter as Router,
     Route,
+    Switch,
     // TODO redirect settings
     Redirect
 } from 'react-router-dom';
 
-import {routes} from './routes'
+import {routes} from './components/common/routes'
 
 import Common from 'Common';
 
@@ -17,6 +18,7 @@ import Profile from "./components/LoggedIn/Profile";
 import AddAccount from "./components/LoggedIn/AddAccount";
 import AccountSettings from "./components/LoggedIn/AccountSettings";
 import StatisticsContent from "./components/LoggedIn/StatisticsContent";
+import OptionsML from "./components/LoggedIn/OptionsML";
 import AccountsList from "./components/LoggedIn/AccountsList";
 import AccountFill from "./components/LoggedIn/AccountFill";
 import RivalSubscribe from "./components/LoggedIn/RivalSubscribe";
@@ -28,24 +30,33 @@ import Login from "./components/LoggedOut/Login";
 const user = {
     loggedIn: false
 };
+const NoMatch = ({ location }) => (
+    <div>
+        <h3>No match for <code>{location.pathname}</code></h3>
+    </div>
+);
 ReactDOM.render(
     <Router>
-        <div className="router">
-            <Common>
-                <Route exact path={routes.root} component={Profile} />
-                <Route exact path={routes.loggedIn.profile} component={Profile} />
-                <Route exact path={routes.loggedIn.settings} component={AccountSettings} />
-                <Route exact path={routes.loggedIn.statistics} component={StatisticsContent} />
-                <Route exact path={routes.loggedIn.addAccount} component={AddAccount} />
-                <Route exact path={routes.loggedIn.accountsList} component={AccountsList} />
-                <Route exact path={routes.loggedIn.accountFill} component={AccountFill} />
-                <Route exact path={routes.loggedIn.rivalSubscribe} component={RivalSubscribe} />
-            </Common>
+            <div className="router">
+                <Common>
+                    <Route exact path={routes.root.path} component={Profile} />
+                    <Route exact path={routes.loggedIn.profile.path} component={Profile} />
+                    <Route exact path={routes.loggedIn.settings.path} component={AccountSettings} />
+                    <Route exact path={routes.loggedIn.statistics.path} component={StatisticsContent} />
+                    <Route exact path={routes.loggedIn.addAccount.path} component={AddAccount} />
+                    <Route exact path={routes.loggedIn.accountsList.path} component={AccountsList} />
+                    <Route exact path={routes.loggedIn.optionsML.path} component={OptionsML} />
+                    <Route exact path={routes.loggedIn.accountFill.path} component={AccountFill} />
+                    <Route exact path={routes.loggedIn.rivalSubscribe.path} component={RivalSubscribe} />
+                </Common>
 
-            <Route exact path={routes.loggedOut.login} component={Login} />
-            <Route exact path={routes.loggedOut.register} component={Registration} />
-            <Route exact path={routes.loggedOut.passForgot} component={ForgotPass} />
-        </div>
+                <Route exact path={routes.loggedOut.login.path} component={Login} />
+                <Route exact path={routes.loggedOut.register.path} component={Registration} />
+                <Route exact path={routes.loggedOut.passForgot.path} component={ForgotPass} />
+
+                {/*<Route path={routes.root} component={NoMatch}/>*/}
+            </div>
+
     </Router>,
   document.getElementById('root')
 );
